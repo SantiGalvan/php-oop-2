8e05aa4708cfb1c3dcc0a769a952329036a97c89
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-26 21:48:33 +0100

    fixed class Type

--- models/Type.php
+++ models/Type.php
@@ -1,11 +1,16 @@
 <?php
 
-class Type extends Category {
+require_once __DIR__ . '/Category.php';
+
+class Type
+{
+    public $category;
     public $type_name;
 
 
-    public function __construct($name, $color, $icon, $type_name) {
-        parent::__construct($name, $color, $icon);
+    public function __construct(Category $category,  $type_name)
+    {
+        $this->category = $category;
         $this->type_name = $type_name;
     }
 }
