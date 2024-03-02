c251478701a5caada7bcb0153e4b3696d1788ad6
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-02 10:45:36 +0100

    added private property & get & set

--- models/Category.php
+++ models/Category.php
@@ -1,13 +1,45 @@
-<?php 
+<?php
 
-class Category {
-    public $name;
-    public $color;
-    public $icon;
+class Category
+{
+    private $name;
+    private $color;
+    private $icon;
 
-    public function __construct($name, $color, $icon) {
+    public function __construct($name, $color, $icon)
+    {
+        $this->setName($name);
+        $this->setColor($color);
+        $this->setIcon($icon);
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
         $this->name = $name;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setColor($color)
+    {
         $this->color = $color;
+    }
+
+    public function getIcon()
+    {
+        return $this->icon;
+    }
+
+    public function setIcon($icon)
+    {
         $this->icon = $icon;
     }
 }
