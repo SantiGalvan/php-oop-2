4472c7c867e2569c1087fadea92b6fea0764b170
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-02 13:07:36 +0100

    created new class AccessoryProduct

---
+++ models/products/AccessoryProduct.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__ . '/Product.php';
+
+class AccessoryProduct extends Product
+{
+    private $color;
+    private $size;
+    public static $type = 'accessory';
+    public static $icon = 'fas fa-bone';
+
+    public function __construct($name, $image, $price, Category $category, $color, $size)
+    {
+        parent::__construct($name, $image, $price, $category);
+        $this->setSize($size);
+        $this->setColor($color);
+    }
+
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    public function setSize($size)
+    {
+        $this->size = $size;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setcolor($color)
+    {
+        $this->color = $color;
+    }
+}
